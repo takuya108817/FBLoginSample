2ff8fbd774c8cdf73e1063413f05f20baef61a01
Takuya Matsuda <>
2018-11-08 19:42:35 +0900

    BreakPointで止めながらだと動くときがある

--- FBLoginSample/MainViewController.swift
+++ FBLoginSample/MainViewController.swift
@@ -19,64 +19,56 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         returnUserData()
-      }
-
-    @IBAction func logout(sender: AnyObject) {
+    }
+    
+    @IBAction func logout(_ sender: AnyObject) {
         let loginManager : FBSDKLoginManager = FBSDKLoginManager()
         loginManager.logOut()
-        self.dismissViewControllerAnimated(true, completion: nil)
+        self.dismiss(animated: true, completion: nil)
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    
-    func returnUserData()
-    {
-        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me",
-            parameters: ["fields": "id, name, first_name, last_name, picture.type(large), email"])
-            graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
-            if ((error) != nil)
-            {
+    func returnUserData() {
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(
+            graphPath: "me",
+            parameters: ["fields": "id, name, first_name, last_name, picture.type(large), email"]
+        )
+        graphRequest.start(completionHandler: { (connection, result, error) -> Void in
+            if ((error) != nil) {
                 // エラー処理
-                println("Error: \(error)")
-            }
-            else
-            {
+                print("Error: \(String(describing: error))")
+            } else {
                 // プロフィール情報をディクショナリに入れる
-                self.userProfile = result as! NSDictionary
-                println(self.userProfile)
-
+                self.userProfile = result as? NSDictionary
+                print(self.userProfile)
+                
                 // プロフィール画像の取得（よくあるように角を丸くする）
-                let profileImageURL : String = self.userProfile.objectForKey("picture")?.objectForKey("data")?.objectForKey("url") as! String
-                var profileImage = UIImage(data: NSData(contentsOfURL: NSURL(string: profileImageURL)!)!)
+                let profileImageURL: String = ((self.userProfile.object(forKey: "picture") as AnyObject)
+                    .object(forKey: "data") as AnyObject)
+                    .object(forKey: "url") as! String
+                
+                let profileImage = UIImage(data: NSData(contentsOf: NSURL(string: profileImageURL)! as URL)! as Data)
                 self.userImage.clipsToBounds = true
                 self.userImage.layer.cornerRadius = 60
-                self.userImage.image = self.trimPicture(profileImage!)
-
-                //名前とemail
+//                self.userImage.image = self.trimPicture(rawPic: profileImage!)
+                self.userImage.image = profileImage
                 
-                self.currentUserName.text = self.userProfile.objectForKey("name") as? String
-                self.currentUserEmail.text = self.userProfile.objectForKey("email") as? String
-
-                }
+                //名前とemail
+                self.currentUserName.text = self.userProfile.object(forKey: "name") as? String
+                self.currentUserEmail.text = self.userProfile.object(forKey: "email") as? String
+            }
         })
-    
     }
     
     func trimPicture(rawPic:UIImage) -> UIImage {
-        var rawImageW = rawPic.size.width
-        var rawImageH = rawPic.size.height
-            
-        var posX = (rawImageW - 200) / 2
-        var posY = (rawImageH - 200) / 2
-        var trimArea : CGRect = CGRectMake(posX, posY, 200, 200)
-            
-        var rawImageRef:CGImageRef = rawPic.CGImage
-        var trimmedImageRef = CGImageCreateWithImageInRect(rawImageRef, trimArea)
-        var trimmedImage : UIImage = UIImage(CGImage : trimmedImageRef)!
+        let rawImageW = rawPic.size.width
+        let rawImageH = rawPic.size.height
+        //        let posX = (rawImageW - 200) / 2
+        //        let posY = (rawImageH - 200) / 2
+        //        let trimArea: CGRect = CGRect(x: posX, y: posY, width: 200, height: 200)
+        let rawImageRef: CGImage = rawPic.cgImage! // ??
+        let trimmedImageRef = CGImage.cropping(rawImageRef)
+        //        var trimmedImageRef = CGImageCreateWithImageInRect(rawImageRef, trimArea)
+        let trimmedImage : UIImage = UIImage(cgImage : trimmedImageRef as! CGImage)
         return trimmedImage
     }
 }
--- FBLoginSample/WelcomeViewController.swift
+++ FBLoginSample/WelcomeViewController.swift
@@ -8,19 +8,18 @@ import FBSDKCoreKit
 import FBSDKLoginKit
 
 class WelcomeViewController: UIViewController, FBSDKLoginButtonDelegate {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    override func viewDidAppear(animated: Bool) {
-        if (FBSDKAccessToken.currentAccessToken() != nil) {
-            println("User Already Logged In")
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if (FBSDKAccessToken.current() != nil) {
+            print("User Already Logged In")
             //メイン画面へ遷移
-            self.performSegueWithIdentifier("showMain", sender: self)
-            
+            self.performSegue(withIdentifier: "showMain", sender: self)
         } else {
-            println("User not Logged In")
+            print("User not Logged In")
             let loginView : FBSDKLoginButton = FBSDKLoginButton()
             self.view.addSubview(loginView)
             loginView.center = self.view.center
@@ -29,34 +28,24 @@ class WelcomeViewController: UIViewController, FBSDKLoginButtonDelegate {
         }
     }
     
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     //ログインボタンが押された時の処理。Facebookの認証とその結果を取得する
-    func loginButton(loginButton: FBSDKLoginButton!,didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
-        println("User Logged In")
-        if ((error) != nil)
-        {
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error?) {
+        print("User Logged In")
+        if ((error) != nil) {
             //エラー処理
         } else if result.isCancelled {
             //キャンセルされた時
         } else {
             //必要な情報が取れていることを確認(今回はemail必須)
-            if result.grantedPermissions.contains("email")
-            {
-             // 次の画面に遷移
-                self.performSegueWithIdentifier("showMain", sender: self)
+            if result.grantedPermissions.contains("email") {
+                // 次の画面に遷移
+                self.performSegue(withIdentifier: "showMain", sender: self)
             }
         }
-        
     }
     
-    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
-        println("User Logged Out")
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("User Logged Out")
     }
-
 }
 
